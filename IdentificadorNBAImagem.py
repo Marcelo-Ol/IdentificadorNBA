fd2b3ad46f99528c6cbf7390b6ac6a3a802b1fc7
Marcelo <marcelo-silvestre2011@hotmail.com>
2025-01-31 14:06:46 -0300

    Adiciona pré-processamento e centralização

--- IdentificadorNBAImagem.py
+++ IdentificadorNBAImagem.py
@@ -4,9 +4,11 @@ import pytesseract
 from PIL import Image
 import pandas
 
-image = cv2.imread("images/Celtics_1.jpg")
+pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 model = YOLO('yolov8n.pt')
 jogadores = pandas.read_csv('BostonCelticsRoster.csv')
+image = cv2.imread("images/Celtics_1.jpg")
+altura, largura = image.shape[:2]
 
 results = model.predict(image)
 boxes = results[0].boxes.xyxy.cpu().numpy()
@@ -14,12 +16,25 @@ boxes = results[0].boxes.xyxy.cpu().numpy()
 for box in boxes:
     x1, y1, x2, y2 = map(int, box)
 
+    # ignora pessoas na arquibancada
+    altura = y2 - y1
+    if y2 < altura * 0.25:
+        continue
+    centro = (y1 + y2) / 2
+    if centro < altura * 0.3:
+        continue
+
     # recorta região do torso
-    torso = image[y1:y2, x1:x2]
+    torso = image[y1:y1+100, x1:x2]
     cv2.imwrite("recorte_torso.jpg", torso)
 
+    # pré-processamento
+    torso_resize = cv2.resize(torso, (300, 100))
+    gray = cv2.cvtColor(torso_resize, cv2.COLOR_BGR2GRAY)
+    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+
     # extrai o número da camisa
-    numero = pytesseract.image_to_string(Image.fromarray(torso), config='--psm 6').strip()
+    numero = pytesseract.image_to_string(Image.fromarray(thresh), config='--psm 8 -c tessedit_char_whitelist=0123456789').strip()
     print(f"Número: '{numero}'")
 
     # consulta número no csv
